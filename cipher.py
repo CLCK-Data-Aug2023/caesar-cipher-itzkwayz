ea9201c035d8fc6a77811a83e0033f71a7303f41
Alen <itzkwayz@hotmail.com>
2023-11-12 13:48:08 -0500

    Edited

--- cipher.py
+++ cipher.py
@@ -1,5 +1,5 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-new_message= ""
+encrpyted_message= ""
 
 message = input("Enter your secret message:").lower() #print(message)
 shift = 5
@@ -9,7 +9,7 @@ for character in message:
         position = alphabet.find(character)
         new_position = (position + shift) % 26
         new_character = alphabet[new_position]
-        new_message += new_character
+        encrpyted_message += new_character
     else:
-        new_message += character
-print("Your new message is " + new_message)
+        encrpyted_message += character
+print("Your encrpyted message is " + encrpyted_message) #print(encrpyted message)
