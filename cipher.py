f5ac5d1a28f103c3fa38083ba8b511e04dcd9099
Alen <itzkwayz@hotmail.com>
2023-11-12 13:20:54 -0500

    Code added

--- cipher.py
+++ cipher.py
@@ -1,1 +1,15 @@
-# add your code here
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+new_message= ""
+
+message = input("Enter your secret message:").lower() #print(message)
+shift = 5
+
+for character in message:
+    if character in alphabet:
+        position = alphabet.find(character)
+        new_position = (position + shift) % 26
+        new_character = alphabet[new_position]
+        new_message += new_character
+    else:
+        new_message += character
+print("Your new message is " + new_message)
